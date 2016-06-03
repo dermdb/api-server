4854baf38d8a752525a2b3bddac28c4bbfcf75cf
Sam Mills (Henchman) <sam.mills@hench.space>
2016-06-03 09:52:39 +0100

    Added image upload method

--- routes/images/index.js
+++ routes/images/index.js
@@ -8,8 +8,29 @@
 
 var express = require('express');
 var router = express.Router();
-var config = require('auto-config');
+var mysql = require("../../interfaces/mysql.js");
 
+var multer  = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        console.log(file);
+        mysql.query("SELECT * FROM snapshots WHERE sid = ")
+        callback(null, __dirname + "/public/" + req.body.pid + "/" + req.body.sid);
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+});
+var upload = multer({ storage : storage}).any();
 
+router.post('/',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            console.error(err);
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+});
 
 module.exports = router;
